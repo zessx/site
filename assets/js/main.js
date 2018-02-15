171148cdece443aedd45bb6e398533c3927ee15a
zessx <contact@zes.sx>
2018-02-15 15:01:45 +0100

    Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 var lastScroll = 0;
 var ticking    = false;
 var blurMax    = 7;
-var blurLimit  = 200;
+var blurLimit  = 50;
 
 function blurry(scroll) {
   blur = Math.min(blurLimit, scroll) * blurMax / blurLimit;
   document.querySelector('main').style.filter = 'blur('+ blur +'px)';
+  opacity = Math.min(blurLimit, scroll) * 0.2 / blurLimit;
+  document.querySelector('#background').style.opacity = 0.3 - opacity;
 }
 
 window.addEventListener('scroll', function(event) {
